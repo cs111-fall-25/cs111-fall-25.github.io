b255efa56b2db7ecf6fd21e787d7cd8575dfa400
Edward Rees <erees.hk@gmail.com>
2025-07-22 11:14:13 -0700

    [fix] updated basenames

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/client",
+  base: "/client/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
