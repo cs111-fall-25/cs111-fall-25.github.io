48fc9278fe6339487308336aa132a3d38bba10af
Edward Rees <erees.hk@gmail.com>
2025-08-05 09:46:01 -0700

    [fix] updated colors

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Navbar, Loading } from "@/components";
 
 export default function LoadingScreen(){
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-100">
+    <div className="min-h-screen bg-gray-950 text-gray-100">
     <Navbar />
     <Loading />
     </div>
--- app/routes/resources/page.tsx
+++ app/routes/resources/page.tsx
@@ -5,7 +5,7 @@ const ResourcePage = () => {
   return (
     <>
     <Navbar />
-    <div className="bg-gray-900 flex justify-center items-center min-h-[88vh]">
+    <div className="bg-gray-950 flex justify-center items-center min-h-[88vh]">
     <p className="text-2xl text-gray-100">Coming Soon</p>
     </div>
     </>
