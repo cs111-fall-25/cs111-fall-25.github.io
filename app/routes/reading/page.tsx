7d9aa23a54124da908e661f6aabca15b17cd6d76
Edward Rees <erees.hk@gmail.com>
2025-08-28 18:22:43 -0700

    [feat] added reading and next week

--- app/routes/reading/page.tsx
+++ app/routes/reading/page.tsx
@@ -52,20 +52,24 @@ const SuggestedReadingPage = () => {
         </ul>
         </TableCell>
         </TableRow>
+        <TableRow>
+        <TableCell className="text-center">2</TableCell>
+        <TableCell className="pl-5 py-5">
+        <p>Suggested for Wednesday:</p>
+          <ul className="list-disc pl-5">
+          <li>
+          <a href="https://www.w3schools.com/java/java_user_input.asp" target="_blank">W3Schools Article: Java User Input</a>
+          </li>
+          <li>
+          <a href="https://www.geeksforgeeks.org/scanner-class-in-java" target="_blank">GeeksForGeeks Article: Scanner</a>
+          </li>
+        </ul>
+        </TableCell>
+        </TableRow>
         {/* <TableRow> */}
-        {/* <TableCell className="text-center">2</TableCell> */}
+        {/* <TableCell className="text-center">3</TableCell> */}
         {/* <TableCell className="pl-5 py-5"> */}
-        {/* <p>Suggested for Monday:</p> */}
-        {/*   <ul className="list-disc pl-5"> */}
-        {/*   <li> */}
-        {/*   <a href="https://www.w3schools.com/java/java_user_input.asp" target="_blank">W3Schools Article: Java User Input</a> */}
-        {/*   </li> */}
-        {/*   <li> */}
-        {/*   <a href="https://www.geeksforgeeks.org/scanner-class-in-java" target="_blank">GeeksForGeeks Article: Scanner</a> */}
-        {/*   </li> */}
-        {/* </ul> */}
-        {/* <br /> */}
-        {/* <p>Suggested for Wednesday: Java Software Solutions</p> */}
+        {/* <p>Monday: Java Software Solutions</p> */}
         {/*   <ul className="list-disc pl-5"> */}
         {/* <li>5.1: Boolean Expressions</li> */}
         {/* <li>5.2: The If Statement</li> */}
@@ -73,6 +77,69 @@ const SuggestedReadingPage = () => {
         {/* <li>5.4: The While Statement</li> */}
         {/* <li>5.5: Iterators</li> */}
         {/* </ul> */}
+        {/* <br /> */}
+        {/* <p>Wednesday: Java Software Solutions</p> */}
+        {/* <ul className="list-disc pl-5"> */}
+        {/* <li>6.4: The for statement</li> */}
+        {/* </ul> */}
+        {/* </TableCell> */}
+        {/* </TableRow> */}
+        {/* <TableRow> */}
+        {/* <TableCell className="text-center">4</TableCell> */}
+        {/* <TableCell className="pl-5 py-5"> */}
+        {/* <p>Monday: Java Software Solutions</p> */}
+        {/* <ul className="list-disc pl-5"><li>3.2:  The String class</li></ul>         */}
+        {/* <br /> */}
+        {/* <p>Wednesday</p> */}
+        {/* <ul className="list-disc pl-5"> */}
+        {/*   <li><a href="https://www.geeksforgeeks.org/dsa/stack-vs-heap-memory-allocation/" target="_blank">GeeksForGeeks Stack v Heap</a></li> */}
+        {/*   <li><a href="https://www.geeksforgeeks.org/java/garbage-collection-in-java/" target="_blank">GeeksForGeeks Garbage Collection</a></li> */}
+        {/*   <li><a href="https://www.geeksforgeeks.org/java/stringbuilder-class-in-java-with-examples/" target="_blank">GeeksForGeeks String Builder</a></li> */}
+        {/*   <li><a href="https://www.w3schools.com/java/java_arrays.asp" target="_blank">W3Schools Java Arrays</a></li> */}
+        {/* </ul> */}
+        {/* </TableCell> */}
+        {/* </TableRow> */}
+        {/* <TableRow> */}
+        {/* <TableCell className="text-center">5</TableCell> */}
+        {/* <TableCell className="pl-5 py-5"> */}
+        {/* <p>Monday: Java Software Solutions</p> */}
+        {/* <ul className="list-disc pl-5"> */}
+        {/*   <li>Chapter 8 Arrays</li> */}
+        {/*   <li>8.1 Array Elements</li> */}
+        {/*   <li>8.2 Declaring and Using Arrays</li> */}
+        {/*   <li>8.3 Array of Objects</li> */}
+        {/*   <li>8.6 Two-Dimensional Arrays</li> */}
+        {/* </ul>  */}
+        {/* <br /> */}
+        {/* <p>Wednesday: Java Software Solutions</p> */}
+        {/* <ul className="list-disc pl-5"> */}
+        {/*   <li>Chapter 7 Object-Oriented Design</li> */}
+        {/*   <li>7.1 Software Development Activities</li> */}
+        {/*   <li>7.2 Identifying Classes and Objects</li> */}
+        {/*   <li>7.3 Static Class Members</li> */}
+        {/* </ul> */}
+        {/* </TableCell> */}
+        {/* </TableRow> */}
+        {/* <TableRow> */}
+        {/* <TableCell className="text-center">6</TableCell> */}
+        {/* <TableCell className="pl-5 py-5"> */}
+        {/* <p>Monday and Wednesday: Java Software Solutions</p> */}
+        {/* <ul className="list-disc pl-5"> */}
+        {/* <li>Chapter 4: Writing Classes</li> */}
+        {/* <li>4.1 Classes and Objects Revisited</li> */}
+        {/* <li>4.2 Anatomy of a Class</li> */}
+        {/* <li>4.3 Encapsulation</li> */}
+        {/* <li>4.4 Anatomy of a Method</li> */}
+        {/* <li>4.5 Constructors Revisited</li> */}
+        {/* <li>7.4 Class Relationships</li> */}
+        {/* <li>7.7 Method Design</li> */}
+        {/* </ul> */}
+        {/* </TableCell> */}
+        {/* </TableRow> */}
+        {/* <TableRow> */}
+        {/* <TableCell className="text-center">7</TableCell> */}
+        {/* <TableCell className="pl-5 py-5"> */}
+        {/* <p>No reading; Midterm Prep</p> */}
         {/* </TableCell> */}
         {/* </TableRow> */}
         </TableBody>
