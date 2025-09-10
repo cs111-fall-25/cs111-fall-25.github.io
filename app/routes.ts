fec6ea4767cc5a8cc567052f29a2370e04590bda
Edward Rees <erees.hk@gmail.com>
2025-09-10 10:51:32 -0700

    [feat] added day 5

--- app/routes.ts
+++ app/routes.ts
@@ -19,7 +19,7 @@ export default [
   route("/slides/02", "routes/slides/02/page.tsx"),
   route("/slides/03", "routes/slides/03/page.tsx"),
   route("/slides/04", "routes/slides/04/page.tsx"),
-  // route("/slides/05", "routes/slides/05/page.tsx"),
+  route("/slides/05", "routes/slides/05/page.tsx"),
   // route("/slides/06", "routes/slides/06/page.tsx"),
   // route("/slides/07", "routes/slides/07/page.tsx"),
 ] satisfies RouteConfig;
