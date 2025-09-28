8223ad1b607296a07be7b6c48728206fd4fc35e5
Edward Rees <erees.hk@gmail.com>
2025-09-28 12:54:32 -0700

    [feat] updated assignment spec with images

--- app/components/AssignmentSpecification/index.tsx
+++ app/components/AssignmentSpecification/index.tsx
@@ -2,6 +2,16 @@ import { type AssignmentSpecificationProps } from "./types";
 import markdownStyles from "@/styles/markdown-styles.module.css";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui';
 
+const getMediaType = (link: string): string => {
+  let sections: string[] = link.split(".");
+  let ext: string = sections[sections.length-1];
+  let imageExt: string[] = ["png", "jpg", "jpeg"];
+  if(imageExt.includes(ext)) {
+    return "image";
+  }
+  return "video";
+}
+
 const AssignmentSpecification = ({
   due_date,
   content,
@@ -22,10 +32,15 @@ const AssignmentSpecification = ({
           return(
           <CarouselItem key={i}>
           <p className="text-2xl text-center">{val.title}</p>
+          { getMediaType(val.link) === "video" ? 
+            (
           <video className="w-screen" width="1024" controls >
           <source src={(val.link)}></source>
-          {/*<!-- Fix the issue with mobile -->*/}
           </video>
+          ) : (
+          <img src={val.link} alt={val.title} className="w-screen" width="1024"/>
+          )
+          }
           </CarouselItem>
           )
         })}
